1aabb46e3e656b4051dbbdfb55b11cf0c6d5b2d3
anyisoft <foo_lw@163.com>
2022-04-19 21:49:22 +0800

    100 factorial right.

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -43,8 +43,8 @@ int main()
     char multiplicand[200];
     char multiplier[4];
     
-    char i, j, k;
-    char cResult;
+    unsigned char i, j, k;
+    unsigned char cResult;
 
     int resultLen = 1;
     int multiplicandLen = 0;
@@ -62,7 +62,7 @@ int main()
 
     memset(result, 0x0, sizeof(result));
     result[0] = 1;
-    for (i = 1; i < 90; i++) {
+    for (i = 1; i < 101; i++) {
         
         memcpy(multiplicand, result, resultLen);
         multiplicandLen = resultLen;
@@ -84,8 +84,8 @@ int main()
             resultLen = multiOneDigit(presultTemp[j], multiplicand, multiplicandLen, multiplier[j]);
 
             if (j > 0) {
-                for (k = resultLen; k > 0; k--) {
-                    presultTemp[j][k] = presultTemp[j][k - 1];
+                for (k = resultLen-1+j; k > 0; k--) {
+                    presultTemp[j][k] = presultTemp[j][k - j];
                 }
                 for (k = 0; k < j; k++) {
                     presultTemp[j][k] = 0;
@@ -94,7 +94,7 @@ int main()
                 resultLen += j;
             }
             
-            std::cout << "temp sub result: ";
+            std::cout << (int)i << "'s sub result_" << (int)j << ": ";
             for (int rIndex = resultLen - 1; rIndex > -1; rIndex--) {
                 std::cout << char(presultTemp[j][rIndex] + '0');
             }
@@ -107,7 +107,7 @@ int main()
         resultLen = sumTempResult(result, presultTemp, j, resultLen);
 
 
-        std::cout << "temp result: ";
+        std::cout << (int)i <<  "'s factorial is: ";
         for (int rIndex = resultLen - 1; rIndex > -1; rIndex--) {
             std::cout << char(result[rIndex] + '0');
         }
@@ -178,86 +178,3 @@ int sumTempResult(char result[], char* presultTemp[], char trNum, int resultLen)
 
     return resultLenNew;
 }
-
-
-/*
-int main()
-{
-    std::cout << "Hello Factorial!\n";
-
-    char multiplicand[4];
-    char multiplier[4];
-    char result[200];
-    char result1[200];
-    char result2[200];
-    char result3[200];
-    char carry[1];
-    char carryOld[1];
-
-    char* paResultTemp[3] = { result3, result2, result1 };
-
-    int multiplicandLen = 1;
-    int multiplierLen = 1;
-    int resultLen = 1;
-
-    int i1;
-    int i2;
-    int iResult;
-    int iCarry, iCarryOld, offset;
-
-    int i, j, k, m;
-
-
-    memset(result, 0x0, sizeof(result));
-    result[0] = 1;
-    carry[0] = 0;
-    carryOld[0] = 0;
-
-    for (i = 1; i < 101; i++) {
-        memset(multiplicand, 0x0, sizeof(multiplicand));
-        memcpy(multiplicand, result, resultLen);
-        memset(multiplier, 0x0, sizeof(multiplier));
-        _itoa_s(i, multiplier, 10);
-
-        memset(result1, 0x0, sizeof(result1));
-        memset(result2, 0x0, sizeof(result2));
-        memset(result3, 0x0, sizeof(result3));
-
-        multiplierLen = strlen(multiplier);
-        for (j = multiplierLen-1; j > -1; j--) {
-            i2 = multiplier[j] - '0';
-            offset = multiplierLen - 1 - j;
-            for (k = 0; k < multiplicandLen; k++) {
-                i1 = multiplicand[k];
-                iResult = i1 * i2;
-                carryOld[0] = carry[0];
-                iCarry = iResult / 10;
-                iResult %= 10;
-
-                paResultTemp[j][k + offset] = iResult + carryOld[0];
-                carry[0] = iCarry;
-            }
-
-            // last bit
-            paResultTemp[j][k + offset] = carry[0];
-        }
-
-
-
-        for (m = 0; m < 3; m++) {
-        }
-
-    }
-
-}
-*/
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
